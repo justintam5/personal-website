a54e9f051b3f937b394e0d6070f1a3ab20a3a5ec
Justin Tam <jt.tam514@gmail.com>
2024-10-11 20:08:56 -0300

    test3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
+import Image from "next/image";
 export default function Home() {
   return (
     <main className="grid min-h-full place-items-center">
-      <div className="flex flex-row flex-wrap justify-center gap-10 content-center">
-        <div className="flex flex-col gap-6 content-center justify-center">
-          <div>
-            <h1 className="mt-10 m-40 text-4xl font-bold tracking-tight text-gray-600 sm:text-8xl">
-              Please send a teamviewer link UwU Ow&lt;
-            </h1>
-          </div>
+      <div className="flex justify-center items-center h-screen m-20">
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="m-20 text-4xl font-bold tracking-tight text-gray-600 sm:text-8xl">
+            Please send a teamviewer link UwU Ow&lt;
+          </h1>
+          <Image
+            src="/melo.jpeg"
+            width={700}
+            height={700}
+            alt="Image from https://www.avogado6.com/"
+            className=""
+          />
         </div>
       </div>
     </main>
